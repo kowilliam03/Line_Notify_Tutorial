3d291c5f0562b876340cc038dfd98ee8747d0a29
kwn <kowilliam03@gmail.com>
2020-05-05 14:36:51 +0800

    modify app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ def send_line_notify_message(token, message):
         "Content-Type": "application/x-www-form-urlencoded"
     }
 
-    payload = {
+    params = {
         "message": message
     }
 
     r = requests.post(" https://notify-api.line.me/api/notify",
-                      headers=header, params=payload)
+                      headers=header, params=params)
 
     return r.status_code
 
